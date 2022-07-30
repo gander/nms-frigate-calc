81172e1511469181e2b5a0c4dd76eb21a73f6dc9
Adam Gąsowski <adam.gasowski@gander.pl>
2022-07-30 16:23:51 +0200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
-import {/*computed,*/ ref, watch} from 'vue';
+import {ref, watch} from 'vue';
 
 const levelUps = new Map([
   [1, 4],
@@ -18,16 +16,11 @@ const levelUps = new Map([
 ]);
 const regex = /\+?\d+|-\d+/g;
 
-// const minFuel = computed(() => support.value ? 2 : 8);
-// const maxFuel = computed(() => support.value ? 5 : 12);
-
 const traitsInput = ref('');
 const combat = ref(0);
 const exploration = ref(0);
 const industry = ref(0);
 const trade = ref(0);
-// const support = ref(false);
-// const fuel = ref(minFuel.value);
 const baseStat = ref(0);
 const expeditions = ref(0);
 
@@ -41,8 +34,6 @@ const reset = () => {
   exploration.value = 0;
   industry.value = 0;
   trade.value = 0;
-  // support.value = false;
-  // fuel.value = minFuel.value;
   baseStat.value = 0;
   expeditions.value = 0;
   stats.value = 0;
@@ -79,16 +70,6 @@ const calcExpeditions = (expeditions: number): number => {
   return result;
 };
 
-
-// watch(support, () => {
-//   if (fuel.value > maxFuel.value) {
-//     fuel.value = maxFuel.value;
-//   }
-//   if (fuel.value < minFuel.value) {
-//     fuel.value = minFuel.value;
-//   }
-// });
-
 watch([traitsInput, combat, exploration, industry, trade, /*support,*/ expeditions], () => {
   stats.value = (combat.value + exploration.value + industry.value + trade.value);
   bonuses.value = extractTraits(traitsInput.value).reduce((sum, cur) => cur + sum, 0);
@@ -99,10 +80,6 @@ watch([traitsInput, combat, exploration, industry, trade, /*support,*/ expeditio
 </script>
 
 <template>
-<!--  <div>-->
-<!--    <label for="support">Support</label>-->
-<!--    <input type="checkbox" id="support" v-model="support"/>-->
-<!--  </div>-->
   <div>
     <label for="expeditions">Expeditions</label>
     <input type="number" min="0" id="expeditions" v-model="expeditions"/>
@@ -123,10 +100,6 @@ watch([traitsInput, combat, exploration, industry, trade, /*support,*/ expeditio
     <label for="trade">Trade</label>
     <input type="number" min="0" id="trade" v-model="trade"/>
   </div>
-<!--  <div>-->
-<!--    <label for="fuel">Fuel</label>-->
-<!--    <input type="number" :min="minFuel" :max="maxFuel" id="trade" v-model="fuel"/>-->
-<!--  </div>-->
   <div>
     <label for="traits">Traits</label>
     <input type="text" id="traits" v-model.trim="traitsInput" inputmode="numeric" pattern="[0-9]*"/>
@@ -141,4 +114,3 @@ watch([traitsInput, combat, exploration, industry, trade, /*support,*/ expeditio
     <h3>SCORE: {{ baseStat }}</h3>
   </div>
 </template>
-
