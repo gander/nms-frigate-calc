77258b61ee89ed67b313edcd6723eeceabd7b7a2
Adam Gąsowski <adam.gasowski@gander.pl>
2023-06-15 15:11:40 +0200

    replace score by colored progressbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import {Ref, ref, watch} from 'vue';
+import {reactive, Ref, ref, watch} from 'vue';
 
 const levelUps = new Map([
   [1, 4],
@@ -19,16 +19,21 @@ const regex = /\+?\d+|-\d+/g;
 const traitsInput = ref('');
 const statsInput = ref('');
 const baseStat = ref(0);
+const validStat = ref(0);
 const expeditions: Ref<number | null> = ref(null);
 
 const stats = ref(0);
 const bonuses = ref(0);
 const levelUp = ref(0);
 
+const scoreClass = ref('');
+const scoreStyle = reactive({width: '10%'});
+
 const reset = () => {
   traitsInput.value = '';
   statsInput.value = '';
   baseStat.value = 0;
+  validStat.value = 0;
   expeditions.value = null;
   stats.value = 0;
   bonuses.value = 0;
@@ -69,7 +74,31 @@ watch([traitsInput, statsInput, expeditions], () => {
   bonuses.value = extractTraits(traitsInput.value).reduce((sum, cur) => cur + sum, 0);
   levelUp.value = calcExpeditions(expeditions.value ?? 0);
   baseStat.value = stats.value - bonuses.value - (6 * levelUp.value);
+  if (traitsInput.value && statsInput.value && expeditions.value) {
+    validStat.value = Math.max(-5, Math.min(14, baseStat.value));
+  } else {
+    validStat.value = 0;
+  }
 });
+
+watch(validStat, (value) => {
+
+      value += 5; // for easy math
+
+      if (value < 5) {
+        scoreClass.value = 'bg-danger';
+      } else if (value < 10) {
+        scoreClass.value = 'bg-warning';
+      } else if (value < 15) {
+        scoreClass.value = 'bg-info';
+      } else {
+        scoreClass.value = 'bg-success';
+      }
+
+      scoreStyle.width = `${Math.round((value * (100 - 10)) / (14 - (-5))) + 10}%`;
+    },
+)
+;
 
 </script>
 
@@ -114,8 +143,13 @@ watch([traitsInput, statsInput, expeditions], () => {
           <span class="input-group-text" title="Number of levels up">{{ levelUp }}</span>
         </div>
 
-        <div class="mb-3 bt-5 text-center fw-bold">
-          <h3>SCORE: <span class="badge rounded-pill bg-success m-3">{{ baseStat }}</span></h3>
+        <div class="my-3 text-center fw-bold">
+          <div class="progress" style="height: 2rem">
+            <template v-if="validStat">
+            <div class="progress-bar" role="progressbar" :class="scoreClass" :style="scoreStyle" aria-label="Score" :aria-valuenow="baseStat" aria-valuemin="-5" aria-valuemax="14">{{ validStat }}</div>
+            </template>
+            <div class="align-self-center flex-fill" v-else>Provide valid stats/traits/expeditions values</div>
+          </div>
         </div>
 
         <div class="mb-3 text-center d-grid">
