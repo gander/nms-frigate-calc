0287014004da180e644a62e76f70d0aaca69dc82
Adam Gąsowski <adam.gasowski@gander.pl>
2023-09-13 21:21:43 +0200

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,48 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
 import htmlPlugin from 'vite-plugin-html-config';
+import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => {
     process.env.VITE_APP_VERSION = process.env.npm_package_version;
 
-    const plugins = [vue()];
+    const plugins = [vue(), VitePWA({
+        includeAssets: ['favicon-32x32.png', 'favicon-16x16.png', 'apple-touch-icon.png', 'robots.txt'],
+        manifest: {
+            name: 'NMS Frigate Calc',
+            short_name: 'NMS-FC',
+            description: 'No Man\'s Sky Frigate Calculator',
+            theme_color: '#ffffff',
+            icons: [
+                {
+                    src: 'android-chrome-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                },
+                {
+                    src: 'android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'any',
+                },
+                {
+                    src: 'android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                    purpose: 'maskable',
+                },
+            ]
+        },
+        devOptions: {
+            enabled: true
+        }
+    })];
 
     if (command === 'build') {
         if (process.env.VITE_UMAMI_ID && process.env.VITE_UMAMI_SRC) {
