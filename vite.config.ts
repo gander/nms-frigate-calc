bcdebe9ea8a2c8d2c2a60a587385e1e2c2105ea4
Adam Gąsowski <gander@users.noreply.github.com>
2024-01-26 01:42:56 +0100

    build: Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,25 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import htmlPlugin from 'vite-plugin-html-config';
+import HtmlPlugin, {ScriptTag} from 'vite-plugin-html-config';
 import {VitePWA} from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => {
     process.env.VITE_APP_VERSION = process.env.npm_package_version;
 
-    const plugins = [vue(), VitePWA({
+    const headScripts: ScriptTag[] = [];
+
+    if (command === 'build') {
+        if (process.env.VITE_UMAMI_ID && process.env.VITE_UMAMI_SRC) {
+            headScripts.push({
+                'async': true,
+                'data-website-id': process.env.VITE_UMAMI_ID,
+                'src': process.env.VITE_UMAMI_SRC,
+            });
+        }
+    }
+
+    const pwaOptions = {
         includeAssets: ['favicon-32x32.png', 'favicon-16x16.png', 'apple-touch-icon.png', 'robots.txt'],
         manifest: {
             id: 'dev.gander.nms-frigate-calc',
@@ -38,24 +50,18 @@ export default defineConfig(({command}) => {
                     type: 'image/png',
                     purpose: 'maskable',
                 },
-            ]
+            ],
         },
         devOptions: {
-            enabled: true
-        }
-    })];
+            enabled: true,
+        },
+    };
 
-    if (command === 'build') {
-        if (process.env.VITE_UMAMI_ID && process.env.VITE_UMAMI_SRC) {
-            plugins.push(htmlPlugin({
-                headScripts: [{
-                    'async': true,
-                    'data-website-id': process.env.VITE_UMAMI_ID,
-                    'src': process.env.VITE_UMAMI_SRC,
-                }],
-            }));
-        }
-    }
-
-    return {plugins};
+    return {
+        plugins: [
+            vue(),
+            HtmlPlugin({headScripts}),
+            VitePWA(pwaOptions),
+        ],
+    };
 });
