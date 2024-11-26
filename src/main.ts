d4548f2b252a5da9fc49a1425b785476153bfe24
Adam Gąsowski <gander@users.noreply.github.com>
2024-11-26 20:05:24 +0100

    build: reconfigure Sentry

--- src/main.ts
+++ src/main.ts
@@ -8,33 +8,13 @@ const app = createApp(App);
 if (import.meta.env.PROD && import.meta.env.VITE_SENTRY_DSN) {
     Sentry.init({
         app,
-        tunnel: '/bugs-tunnel',
         dsn: import.meta.env.VITE_SENTRY_DSN,
         environment: import.meta.env.MODE,
         integrations: [
             Sentry.replayIntegration(),
-            Sentry.feedbackIntegration({
-                buttonLabel: 'Send Feedback',
-                formTitle: 'Send Feedback',
-                submitButtonLabel: 'Send',
-                successMessageText: 'Thank you for your feedback!',
-                // @ts-ignore
-                onFormOpen: () => umami?.track('Feedback Open'),
-                // @ts-ignore
-                onSubmitSuccess: () => umami?.track('Feedback Send'),
-                // @ts-ignore
-                onSubmitError: () => umami?.track('Feedback Error'),
-            }),
         ],
         replaysSessionSampleRate: 1.0,
         replaysOnErrorSampleRate: 1.0,
-        beforeSend(event) {
-            if (event.exception && event.event_id) {
-                Sentry.showReportDialog({ eventId: event.event_id });
-            }
-            return event;
-        },
-
     });
 }
 
