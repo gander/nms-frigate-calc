af2ee9b1aafe60f9424d65f3e04d7c113fc63ac1
Adam Gąsowski <gander@users.noreply.github.com>
2024-11-26 22:50:06 +0100

    feat: CSP NONCE

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,9 @@ export default defineConfig(({command}) => {
     };
 
     return {
+        html: {
+            cspNonce: '{{CSP-NONCE}}',
+        },
         plugins: [
             vue(),
             HtmlPlugin({headScripts, metas}),
