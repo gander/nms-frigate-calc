5abef6144e86d7cfec0ec545b6fb5536876b64f8
Adam Gąsowski <gander@users.noreply.github.com>
2024-11-26 22:27:27 +0100

    build: reconfigure CSP improvements

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -12,7 +12,8 @@ async function handleNonceResponse(response) {
     const nonce = generateNonce();
 
     let newHeaders = new Headers(response.headers);
-    newHeaders.set('Content-Security-Policy-Report-Only', `script-src https: http: 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic'; object-src 'none'; base-uri 'none'; style-src 'nonce-${nonce}'; require-trusted-types-for 'script'; report-uri https://sentry.gander.tools/api/7/security/?sentry_key=6a9fe9561e987be60e05e9acdb5a770a`);
+    newHeaders.set('Reporting-Endpoints', 'csp-endpoint="https://csp.gander.tools/"')
+    newHeaders.set('Content-Security-Policy-Report-Only', `default-src 'self'; script-src https: http: 'unsafe-inline' 'nonce-${nonce}' 'strict-dynamic'; object-src 'none'; base-uri 'none'; style-src 'nonce-${nonce}'; require-trusted-types-for 'script'; report-to csp-endpoint; report-uri https://csp.gander.tools/`);
 
     let body = await response.text();
     body = body.replace(/{{CSP-NONCE}}/g, nonce);
